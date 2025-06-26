817a22ae03aadc6b83ff4fcc663d49c5552b9ce7
Eternalnacho <ahanshawbhaskar@gmail.com>
2025-06-26 12:46:06 -0500

    Update stakes.lua

--- stakes/stakes.lua
+++ stakes/stakes.lua
@@ -5,12 +5,8 @@ local elite = { -- Elite Stake
     prefix_config = {above_stake = {mod = false}, applied_stakes = {mod = false}},
 
     modifiers = function()
-        if next(SMODS.find_mod("SonfivesPokermonPlus")) and G.GAME.selected_back.effect.center.key == "b_sonfive_megadeck" then
-            G.GAME.modifiers.elite4 = true
-        else
-            G.GAME.win_ante = (G.GAME.win_ante + 2)
-            G.GAME.modifiers.elite4 = true
-        end
+        G.GAME.win_ante = (G.GAME.win_ante + 2)
+        G.GAME.modifiers.elite4 = true
     end,
 
     pos = {x = 0, y = 0},
