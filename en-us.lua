f9c732df17d9778a61e12a3917a2663cf62ae061
Eternalnacho <ahanshawbhaskar@gmail.com>
2025-07-02 23:19:07 -0500

    Add files via upload

---
+++ en-us.lua
@@ -0,0 +1,389 @@
+return {
+    descriptions = {
+        Joker = {
+            j_nacho_ralts = {
+                name = "Ralts",
+                text = {
+                    "{C:red}+#1#{} Mult per {C:attention}hand level",
+                    "above 1 {C:inactive}(Currently {C:red}+#2#{C:inactive} Mult)",
+                    "{C:inactive,s:0.8}(Evolves after {C:attention,s:0.8}#3#{C:inactive,s:0.8} rounds)",
+                } 
+            },
+            j_nacho_kirlia = {
+                name = "Kirlia",
+                text = {
+                    "{C:red}+#1#{} Mult per {C:attention}hand level",
+                    "above 1 {C:inactive}(Currently {C:red}+#2#{C:inactive} Mult)",
+                    "{C:inactive,s:0.8}(Evolves with a {C:attention,s:0.8}Dawn Stone{C:inactive,s:0.8})",
+                    "{C:inactive,s:0.8}(Evolves after {C:attention,s:0.8}#3#{C:inactive,s:0.8} rounds)",
+                } 
+            },
+            j_nacho_gardevoir = {
+                name = "Gardevoir",
+                text = {
+                    "{X:red,C:white}X#1#{} Mult per {C:attention}hand level",
+                    "above 1 {C:inactive}(Currently {X:red,C:white}X#2#{C:inactive} Mult)",
+                } 
+            },
+            j_nacho_mega_gardevoir = {
+                name = "Mega Gardevoir",
+                text = {
+                    "{C:attention}Holding{} {C:dark_edition}Negative{} {C:spectral}Black Hole{}",
+                    "{br:2}ERROR - CONTACT STEAK",
+                    "Create an {C:attention}Orbital Tag{}",
+                    "when a {C:planet}Planet{} card is used",
+                    "{br:2}ERROR - CONTACT STEAK",
+                    "Held {C:planet}Planet{} cards without an edition",
+                    "gain {C:spectral}Polychrome{} at end of round",
+                } 
+            },
+            j_nacho_turtwig = {
+                name = "Turtwig",
+                text = {
+                    "{C:attention}+#1#{} hand size",
+                    "{br:2}ERROR - CONTACT STEAK",
+                    "Raises the cap on interest",
+                    "earned each round by {C:money}$1{}",
+                    "{C:inactive,s:0.8}(Evolves after {C:attention,s:0.8}#2#{C:inactive,s:0.8} rounds)",
+                } 
+            },
+            j_nacho_grotle = {
+                name = "Grotle",
+                text = {
+                    "{C:attention}+1{} hand size for every",
+                    "{C:money}$15{} you have {C:inactive}(Max of 2)",
+                    "{br:2}ERROR - CONTACT STEAK",
+                    "Raises the cap on interest",
+                    "earned each round by {C:money}$2{}",
+                    "{C:inactive,s:0.8}(Evolves after {C:attention,s:0.8}#2#{C:inactive,s:0.8} rounds)",
+                } 
+            },
+            j_nacho_torterra = {
+                name = "Torterra",
+                text = {
+                    "{C:attention}+1{} hand size for every",
+                    "{C:money}$15{} you have {C:inactive}(Max of 4)",
+                    "{br:2}ERROR - CONTACT STEAK",
+                    "Raises the cap on interest",
+                    "earned each round by {C:money}$3{}",
+                } 
+            },
+            j_nacho_chimchar = {
+                name = "Chimchar",
+                text = {
+                    "{C:red}+#1#{} discard",
+                    "Adds the rank of {C:attention}highest{} ranked",
+                    "card discarded this round to Mult",
+                    "{C:inactive}(Currently {C:mult}+#2#{} {C:inactive}Mult)",        
+                    "{C:inactive,s:0.8}(Evolves after scoring {C:red,s:0.8}+11{} {C:inactive,s:0.8}Mult{} {C:attention,s:0.8}#3#{C:inactive,s:0.8} Times){}",
+                }
+            },
+            j_nacho_monferno = {
+                name = "Monferno",
+                text = {
+                    "{C:red}+#1#{} discard",
+                    "Gains Mult equal to the rank of",
+                    "{C:attention}highest{} ranked card discarded,",
+                    "resets at end of round",
+                    "{C:inactive}(Currently {C:mult}+#2#{} {C:inactive}Mult)",
+                    "{C:inactive,s:0.8}(Evolves after scoring {C:red,s:0.8}+30{} {C:inactive,s:0.8}Mult{} {C:attention,s:0.8}#3#{C:inactive,s:0.8} Times){}",
+                }
+            },
+            j_nacho_infernape = {
+                name = "Infernape",
+                text = {
+                    "{C:red}+#1#{} discard, {C:mult}+#2#{} Mult",
+                    "Gains {X:red,C:white}X#3#{} Mult for each",
+                    "{C:attention}face{} card or {C:attention}Ace{}",
+                    "discarded this round",
+                    "{C:inactive}(Currently {X:red,C:white}X#4#{} {C:inactive}Mult)",
+                }
+            },
+            j_nacho_piplup = {
+                name = "Piplup",
+                text = {
+                    "{C:blue}+#1#{} hand, {C:chips}+#2#{} Chips",
+                    "{C:chips}-#3#{} Chips per",
+                    "scoring card in played hand",
+                    "{C:inactive,s:0.8}(Evolves after {C:attention,s:0.8}#4#{C:inactive,s:0.8} rounds)",
+                } 
+            },
+            j_nacho_prinplup = {
+                name = "Prinplup",
+                text = {
+                    "{C:blue}+#1#{} hand, {C:chips}+#2#{} Chips",
+                    "Each card {C:attention}held in hand{}",
+                    "gives Chips equal to its rank",
+                    "{C:inactive,s:0.8}(Evolves after {C:attention,s:0.8}#4#{C:inactive,s:0.8} rounds)",
+                } 
+            },
+            j_nacho_empoleon = {
+                name = "Empoleon",
+                text = {
+                    "{C:blue}+#1#{} hand, {C:chips}+#2#{} Chips",
+                    "Each card {C:attention}held in hand{} gives ",
+                    "Chips equal to {C:attention}double{} its rank",
+                } 
+            },
+            j_nacho_gallade = {
+                name = "Gallade",
+                text = {
+                    "Your most played hand gains three levels",
+                    "every 3 {C:planet}Planet{} cards used {C:inactive}[#1#/3]{}",
+                    "{br:2}ERROR - CONTACT STEAK",
+                    "{C:planet}Planet{} cards do not raise",
+                    "the level of {C:attention}poker hands{}",
+                    "{br:2}ERROR - CONTACT STEAK",
+                    "{C:red}+#2#{} Mult and {X:red,C:white}X#3#{} Mult for each time",
+                    "{C:attention}poker hand{} has been played this run",
+                }
+            },
+            j_nacho_mega_gallade = {
+                name = "Mega Gallade",
+                text = {
+                    "{C:attention}Poker hands{} cannot be debuffed",
+                    "This Joker cannot be debuffed",
+                    -- "{br:2}ERROR - CONTACT STEAK",
+                    -- "Scoring cards cannot be debuffed",
+                    -- "if {C:attention}poker hand{} is your most played hand",
+                    "{br:2}ERROR - CONTACT STEAK",
+                    "{C:red}+#1#{} Mult and {X:red,C:white}X#2#{} Mult for each time",
+                    "{C:attention}poker hand{} has been played this run",
+                }
+            },
+            j_nacho_dedenne = {
+                name = "Dedenne",
+                text = {
+                    "{C:attention}Gold{} cards {C:attention}held in hand{}",
+                    "have a {C:green}1 in #1#{} chance to",
+                    "create an {C:item}Item{} when triggered",
+                }
+            },
+            j_nacho_carbink = {
+                name = "Carbink",
+                text = {
+                    "{C:attention}Hazard{} cards are also",
+                    "considered {C:attention}Gold{} cards",
+                }
+            },
+            j_nacho_goomy = {
+                name = "Goomy",
+                text = {
+                    "If played hand contains a Flush,",
+                    "cards {C:attention}held in hand{} with",
+                    "the {C:attention}same suit{} gain {C:red}+#1#{} Mult",
+                    "{C:inactive,s:0.8}(Evolves after triggering {C:attention,s:0.8}#2#{C:inactive,s:0.8} times)",
+                    "{C:inactive,s:0.8}(Evolves after playing a {C:attention,s:0.8}Flush House{C:inactive,s:0.8})",
+                }
+            },
+            j_nacho_sliggoo = {
+                name = "Sliggoo",
+                text = {
+                    "If played hand is a Flush, scoring cards",
+                    "and cards {C:attention}held in hand{} with",
+                    "the {C:attention}same suit{} gain {C:red}+#1#{} Mult for each",
+                    "unique {C:attention}rank{} among scoring cards",
+                    "{C:inactive,s:0.8}(Evolves after playing {C:attention,s:0.8}#2# Flushes{C:inactive,s:0.8})",
+                }
+            },
+            j_nacho_goodra = {
+                name = "Goodra",
+                text = {
+                    "If played hand is a Flush, scoring cards",
+                    "and cards {C:attention}held in hand{} with",
+                    "the {C:attention}same suit{} gain {X:red,C:white}X#1#{} Mult for each",
+                    "unique {C:attention}rank{} among scoring cards",
+                }
+            },
+            j_nacho_turtonator = {
+                name = "Turtonator",
+                text = {
+                    "If previous hand triggered",
+                    "the {C:attention}Boss Blind{} ability,",
+                    "scoring cards give {X:red,C:white}X#1#{} Mult",
+                    "{C:inactive}(#2#){}",
+                }
+            },
+            j_nacho_skwovet = {
+                name = "Skwovet",
+                text = {
+                    "Gains {C:mult}+#2#{} Mult when a",
+                    "consumable is used",
+                    "during a blind",
+                    "{C:inactive,s:0.8}(Evolves at {C:mult,s:0.8}+#1#{C:inactive,s:0.8} / +12 Mult)",
+                }
+            },
+            j_nacho_greedent = {
+                name = "Greedent",
+                text = {
+                    "Gains {C:mult}+#2#{} Mult and",
+                    "has a {C:green}1 in #3#{} chance to",
+                    "create a {C:dark_edition}Negative{} {C:item}Leftovers{}",
+                    "when a consumable is used",
+                    "during a blind",
+                    "{C:inactive,s:0.8}(Leftovers excluded)",
+                    "{C:inactive}(Currently {C:mult}+#1#{} {C:inactive}Mult)",
+                }
+            },
+            j_nacho_galarian_meowth = {
+                name = "{s:0.6}Galarian{}Meowth",
+                text = {
+                    "Retriggers the first two",
+                    "{C:attention}Steel{} cards {C:attention}held in hand{}",
+                    "{C:inactive,s:0.8}(Evolves when each adjacent Joker",
+                    "{C:inactive,s:0.8}has a {C:metal,s:0.8}Metal{C:inactive,s:0.8} sticker)",
+                }
+            },
+            j_nacho_perrserker = {
+                name = "Perrserker",
+                text = {
+                    "Retriggers the first three",
+                    "{C:attention}Steel{} cards {C:attention}held in hand{}",
+                    "{br:2}ERROR - CONTACT STEAK",
+                    "Each {C:metal}Metal {C:attention}Joker{}",
+                    "gives {X:mult,C:white}X#1#{} Mult",
+                    "{br:2}ERROR - CONTACT STEAK",
+                    "Retriggers if all",
+                    "{C:attention}Jokers{} are {C:metal}Metal{}",
+                }
+            },
+            j_nacho_hisuian_zorua = {
+                name = "{s:0.6}Hisuian{}Zorua",
+                text = {
+                    "{V:1}Copies the ability",
+                    "of leftmost {C:attention}Joker{}",
+                    "{br:2.5}ERROR - CONTACT STEAK",
+                    "After scoring played hand while",
+                    "copying, remove copy effect",
+                    "until end of round",
+                    "{C:inactive,s:0.8}(Evolves after {C:attention,s:0.8}#1#{C:inactive,s:0.8} rounds)",
+                }
+            },
+            j_nacho_hisuian_zoroark = {
+                name = "{s:0.6}Hisuian{}Zoroark",
+                text = {
+                    "Copies the ability",
+                    "of leftmost {C:attention}Joker{}",
+                }
+            },
+            j_nacho_hisuian_sliggoo = {
+                name = "{s:0.6}Hisuian{}Sliggoo",
+                text = {
+                    "If played hand is a {C:attention}Flush House{},",
+                    "creates a {C:item}Metal Coat{}",
+                    "{br:2.5}ERROR - CONTACT STEAK",
+                    "Creates another {C:item}Metal Coat{} if",
+                    "the difference in scoring {C:attention}ranks{} is",
+                    "greater than {C:attention}6{}",
+                    "{C:inactive,s:0.8}(Evolves after playing {C:attention,s:0.8}#1# Flush Houses{C:inactive,s:0.8})",
+                }
+            },
+            j_nacho_hisuian_goodra = {
+                name = "{s:0.6}Hisuian{}Goodra",
+                text = {
+                    "If played hand is a {C:attention}Flush House{},",
+                    "creates a {C:item}Metal Coat{}",
+                    "{br:2.5}ERROR - CONTACT STEAK",
+                    "If played hand is a {C:attention}Flush House{},",
+                    "{C:attention}Steel{} cards {C:attention}held in hand{}",
+                    "give {X:mult,C:white}X{} Mult equal to one third of",
+                    "the difference in scoring {C:attention}ranks{}",
+                }
+            },
+            j_nacho_terapagos = {
+                name = "Terapagos",
+                text = {
+                    "{C:attention}Holding{} {C:dark_edition}Negative{} {C:item}Tera Orb{}",
+                    "Creates a {C:dark_edition}Negative{} {C:item}Tera Orb{} at end of round",
+                    "{C:pink}Energizes{} all Jokers upon transforming",
+                    "{C:inactive,s:0.8}(Transforms after using a {C:item,s:0.8}Tera Orb{C:inactive,s:0.8} on Terapagos)",
+                }
+            },
+            j_nacho_terapagos_terastal = {
+                name = "Terapagos-Terastal",
+                text = {
+                    "{C:pink}+3{} Energy Limit",
+                    "Using a {C:item}Tera Orb{} on Terapagos",
+                    "applies a {C:item}Tera Orb{} to each Joker",
+                    "{br:2.5}ERROR - CONTACT STEAK",
+                    "{X:mult,C:white}X#1#{} Mult for each Energy applied",
+                    "on all Jokers {C:inactive}(Currently {X:mult,C:white}X#2#{C:inactive} Mult)",
+                    "{br:2.5}ERROR - CONTACT STEAK",
+                    "{C:inactive,s:0.8}(Transforms after using a {C:item,s:0.8}Tera Orb{C:inactive,s:0.8} on Terapagos",
+                    "{C:inactive,s:0.8}if it has 6 Energy or more)",
+                }
+            },
+            j_nacho_terapagos_stellar = {
+                name = "Terapagos-Stellar",
+                text = {
+                    "{C:pink}+5{} Energy Limit",
+                    "Each Joker with a Type Sticker", 
+                    "becomes {C:nacho_sg1}S{C:nacho_sg2}t{C:nacho_sg3}e{C:nacho_sg4}l{C:nacho_sg5}l{C:nacho_sg6}a{C:nacho_sg7}r{} Type",
+                    "Using a {C:item}Tera Orb{} on Terapagos",
+                    "applies a {C:item}Tera Orb{} to each Joker",
+                    "{br:2.5}ERROR - CONTACT STEAK",
+                    "Each {C:nacho_sg1}S{C:nacho_sg2}t{C:nacho_sg3}e{C:nacho_sg4}l{C:nacho_sg5}l{C:nacho_sg6}a{C:nacho_sg7}r{} Joker gives {X:mult,C:white}X#1#{} Mult for each",
+                    "Energy applied to it",
+                }
+            },
+        },
+        Stake={
+            stake_nacho_elite_stake = {
+                name = "Elite Stake",
+                text = {
+                    "{X:gray,C:attention}+2{} Ante win requirement",
+                    "Last 4 Antes are Showdown Boss Blinds",
+                    "{s:0.8}Applies all previous Stakes"
+                },
+            },
+        },
+        Other={
+            nacho_elite_stake_sticker = {
+                name = "Elite Sticker",
+                text = {
+                    "Used this {C:attention}Joker{} to win on",
+                    "{C:attention}Elite Stake{} difficulty",
+                },
+            },
+            stellar_type = {
+                name = "Stellar Type",
+                text = {
+                    "{C:nacho_sg1}S{C:nacho_sg2}t{C:nacho_sg3}e{C:nacho_sg4}l{C:nacho_sg5}l{C:nacho_sg6}a{C:nacho_sg7}r{} Jokers are every type",
+                },
+            },
+        },
+    },
+    misc = {
+        challenge_names = {
+            c_nacho_sinnoh_adv = "Sinnoh Adventure",
+            c_nacho_hibernation = "Hibernation",
+            c_nacho_gems = "Precious Stones",
+            c_nacho_goomygoomy = "Slime or Steel",
+        },
+        dictionary = {
+            -- From Turtwig, Grotle, Torterra
+            poke_leech_seed_ex = "Leech Seed!",
+            -- From Infernape
+            poke_close_combat_ex = "Close Combat!",
+            -- From Empoleon
+            poke_brine_ex = "Brine!",
+            -- From Gallade
+            poke_psycho_cut_ex = "Psycho Cut!",
+            -- From Turtonator
+            poke_shell_trap_ex = "Shell Trap!",
+            -- From Greedent
+            poke_stuff_cheeks_ex = "Stuff Cheeks!",
+
+
+            -- Maelmc Badge
+            maelmc = "Pokermon-Maelmc",
+        },
+        v_text = {
+            ch_c_sinnoh_adv={
+                "Sinnoh starter pokemon appear in starting shop",
+            },
+        },
+    },
+}
+
+
