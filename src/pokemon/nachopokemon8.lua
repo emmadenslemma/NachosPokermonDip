15d3e98ffb576dc208a342feab23ac599f3f6e70
Eternalnacho <ahanshawbhaskar@gmail.com>
2025-10-22 12:49:07 -0500

    Greedent Fix

--- src/pokemon/nachopokemon8.lua
+++ src/pokemon/nachopokemon8.lua
@@ -59,7 +59,7 @@ local skwovet={
 -- Greedent 820
 local greedent={
   name = "greedent",
-  config = {extra = {mult = 0, mult_mod = 2, num = 4, den = 4, in_blind = false}},
+  config = {extra = {mult = 0, mult_mod = 2, num = 1, den = 4, in_blind = false}},
   loc_vars = function(self, info_queue, card)
     type_tooltip(self, info_queue, card)
     local num, den = SMODS.get_probability_vars(card, card.ability.extra.num, card.ability.extra.den, 'greedent')
